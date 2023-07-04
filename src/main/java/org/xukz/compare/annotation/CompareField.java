79e99bdc2f1ebda390185a409c21adb2eef4b4e9
Xu <rgba1645@gmail.com>
2023-07-04 15:49:30 +0800

    'init'

---
+++ src/main/java/org/xukz/compare/annotation/CompareField.java
@@ -0,0 +1,20 @@
+package org.xukz.compare.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * @author xukz
+ * @since 1.0
+ */
+@Documented
+@Target(ElementType.FIELD)
+public @interface CompareField {
+    String name();
+
+    String suffix() default "Diff";
+
+    Class<?> fun();
+
+}
