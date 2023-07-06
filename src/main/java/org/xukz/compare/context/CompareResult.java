884016dede1b79531310034416315d8b92785c7c
Xu <rgba1645@gmail.com>
2023-07-06 16:55:57 +0800

    coding

---
+++ src/main/java/org/xukz/compare/context/CompareResult.java
@@ -0,0 +1,34 @@
+package org.xukz.compare.context;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author xukz
+ * @since 1.0
+ */
+public class CompareResult {
+
+    private Map<String, Object> newMap = new LinkedHashMap<>();
+
+    private Map<String, Object> delMap = new LinkedHashMap<>();
+
+    private Map<String, Object> modMap = new LinkedHashMap<>();
+
+
+    public void addResult(String identity, Object value, Type type) {
+        switch (type) {
+            case NEW -> newMap.put(identity, value);
+            case DEL -> delMap.put(identity, value);
+            case MOD -> modMap.put(identity, value);
+        }
+
+    }
+
+    public static enum Type {
+        NEW,
+        DEL,
+        MOD
+    }
+
+}
--- src/main/java/org/xukz/compare/core/CompareTool.java
+++ src/main/java/org/xukz/compare/core/CompareTool.java
@@ -1,14 +1,16 @@
 package org.xukz.compare.core;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.sun.tools.javac.util.Assert;
+import lombok.SneakyThrows;
+import org.apache.commons.lang3.StringUtils;
 import org.xukz.compare.annotation.CompareBody;
 import org.xukz.compare.annotation.CompareField;
+import org.xukz.compare.annotation.Identity;
 
 import java.lang.reflect.Field;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author xukz
@@ -20,6 +22,10 @@ public class CompareTool<T> {
     private Map<String, CompareField> fieldMap = new HashMap<>();
 
     private CompareBody compareBody;
+
+    private String identity;
+
+    private Field identityField;
 
     public CompareTool(Class<T> clazz) {
         Assert.checkNonNull(clazz, "Input parameter class is empty!");
@@ -35,15 +41,52 @@ public class CompareTool<T> {
             if (fieldAnno != null) {
                 fieldMap.put(fieldAnno.name(), fieldAnno);
             }
+
+            Identity identityAnno = field.getAnnotation(Identity.class);
+            if (identityAnno != null) {
+                if (StringUtils.isNotBlank(identity)) {
+                    throw new RuntimeException("Multiple Identity annotations present");
+                }
+                identity = field.getName();
+                identityField = field;
+            }
         });
     }
 
     public Map<String, Object> compare(T source, T target) {
+        Map<String, T> sourceMap = JSONObject.parseObject(JSON.toJSONString(source), Map.class);
+        Map<String, T> targetMap = JSONObject.parseObject(JSON.toJSONString(target), Map.class);
+
 
     }
 
-    public List<Map<String, Object>> compare(List<T> source, List<T> target) {
+    public List<Map<String, T>> compare(List<T> sourceList, List<T> targetList) {
+        Map<Object, T> sourceListMap = parseIdentity(sourceList);
+        Map<Object, T> targetListMap = parseIdentity(targetList);
 
+        Set<Object> exsitIdentitys = new HashSet<>();
+        sourceListMap.forEach((identity, sourceObj) -> {
+            T targetObj = targetListMap.get(identity);
+            if (targetObj == null) {
+
+            } else {
+                compare(sourceObj, targetObj);
+                exsitIdentitys.add(identity);
+            }
+        });
+    }
+
+    @SneakyThrows
+    private Map<Object, T> parseIdentity(List<T> list) {
+        if (list == null) return new HashMap<>();
+        LinkedHashMap<Object, T> map = new LinkedHashMap<>();
+        for (T t : list) {
+            Object key = identityField.get(t);
+            if (key != null) {
+                map.put(key, t);
+            }
+        }
+        return map;
     }
 
 
